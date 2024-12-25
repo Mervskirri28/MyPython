0a786f1a9c8d19c0677eb6e9e6a9795f91c44faf
Mervin Jaurigue <76638174+Mervskirri28@users.noreply.github.com>
2024-12-25 13:09:34 +0800

    Add files via upload

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,9 +1,37 @@
 #while loop = execute some code While some condition remains true
 
 
-name = input("Enter your name: ")
+#name = input("Enter your name: ")
 
-while name == "":
-    print("Error! No submitted entry")
-    name = input("Enter your name: ")
-print(f"Welcome {name}")
+#while name == "":
+   # print("Error! No submitted entry")
+   # name = input("Enter your name: ")
+#print(f"Welcome {name}")
+
+
+#--------------------------------------------------
+
+#age = int(input("Enter your age: "))
+
+#while age < 0:
+    # print("Age can't be negative")
+    # age = int(input("Enter your age: "))
+#print(f"You are {age} year old")
+
+#--------------------------------------------------
+
+#food = input("Enter a food you like (press q to quit): ")
+
+#while not food == "q":
+    #print(f"You like {food}")
+    #food = input("Enter a food you like (press q to quit): ")
+#rint("Bye")
+
+#--------------------------------------------------
+
+num = int(input("Enter a # between 1 - 10: "))
+
+while num <1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
+print(f"Your number is {num}")
